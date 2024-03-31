92d6824e9f4a453a77e05a2647cdd01f9bb88131
ErinaHasesgawa0422 <hasegawa.erina0422@gmail.com>
2024-03-31 23:35:04 +0900

    add test fail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Col, Row } from "react-bootstrap";
+import Login from "./Login";
 
 function App() {
   return (
